ea21460e13ba8ff149a77036c7b7db0a1e1f41c1
Moraniel Junior <moraniel@outlook.com>
2025-01-20 15:37:50 -0300

    Passo 2 realizado, pegando os dados de pokeapi.co

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,42 @@
-console.log("OPa")
+
+
+
+
+
+const pokemonList = document.getElementById("pokemonList")
+
+
+
+
+
+
+PokeAPI.getPokemons().then( (pokemons = [])=>{
+    pokemonList.innerHTML+=pokemons.map(pokemonToLi).join('')
+    
+    })
+        
+
+
+function pokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class ="number">#001</span>
+        <span class ="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+            // alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+}
+
+    // fetch(pokemonList.url)
+    // .finally(()=> console.log("Requisição concluida!"))
+    
+
